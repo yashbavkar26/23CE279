1bd15cdfd78edc7bd7077057f8c88c0a1602188a
yashbavkar26 <yashbavkar26@gmail.com>
2024-04-02 18:23:52 +0530

    strings

---
+++ 23CE279/strings3.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void main()
+{
+	char str1[100], str2[100];
+	printf("Enter your first name:");
+	scanf("%s", str1);
+	printf("\n Enter your last name:");
+	scanf("%s", str2);
+	printf("Your full name is %s", strcat(str1, str2));
+	_getch();
+
+
+}
